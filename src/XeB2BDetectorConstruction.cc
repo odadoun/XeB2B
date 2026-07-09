43029937d7c8d9c7754eceba76b7b826a36c3a92
olivier dadoun <odadoun@gmail.com>
2026-07-09 08:33:14 +0200

    Add colors and transparency to detector volumes

--- src/XeB2BDetectorConstruction.cc
+++ src/XeB2BDetectorConstruction.cc
@@ -7,6 +7,8 @@
 #include "G4PVPlacement.hh"
 #include "G4NistManager.hh"
 #include "G4SystemOfUnits.hh"
+#include "G4VisAttributes.hh"
+#include "G4Colour.hh"
 
 XeB2BDetectorConstruction::XeB2BDetectorConstruction() {}
 
@@ -22,15 +24,33 @@ G4VPhysicalVolume* XeB2BDetectorConstruction::Construct() {
     G4LogicalVolume* logicalWorld = new G4LogicalVolume(solidWorld, nist->FindOrBuildMaterial("G4_AIR"), "World");
     G4VPhysicalVolume* physicalWorld = new G4PVPlacement(0, G4ThreeVector(), logicalWorld, "World", 0, false, 0);
 
+    // World visualization attributes (light blue, transparent)
+    G4VisAttributes* worldAttributes = new G4VisAttributes(G4Colour(0.5, 0.8, 1.0, 0.1)); // RGBA
+    worldAttributes->SetVisibility(true);
+    worldAttributes->SetDaughtersInvisible(false);
+    logicalWorld->SetVisAttributes(worldAttributes);
+
     // Cylindre externe (Xénon)
     G4Tubs* solidOuterXenon = new G4Tubs("OuterXenon", 0., 0.5*m, 1.0*m, 0., 360.*deg);
     logicalOuterXenon = new G4LogicalVolume(solidOuterXenon, xenon, "OuterXenon");
     new G4PVPlacement(0, G4ThreeVector(), logicalOuterXenon, "OuterXenon", logicalWorld, false, 0);
 
+    // Outer xenon visualization attributes (cyan, semi-transparent)
+    G4VisAttributes* outerXenonAttributes = new G4VisAttributes(G4Colour(0.0, 1.0, 1.0, 0.3)); // Cyan
+    outerXenonAttributes->SetVisibility(true);
+    outerXenonAttributes->SetLineWidth(2);
+    logicalOuterXenon->SetVisAttributes(outerXenonAttributes);
+
     // Cylindre interne (Xénon)
     G4Tubs* solidInnerXenon = new G4Tubs("InnerXenon", 0., 0.2*m, 0.5*m, 0., 360.*deg);
     logicalInnerXenon = new G4LogicalVolume(solidInnerXenon, xenon, "InnerXenon");
     new G4PVPlacement(0, G4ThreeVector(), logicalInnerXenon, "InnerXenon", logicalWorld, false, 0);
+
+    // Inner xenon visualization attributes (green, semi-transparent)
+    G4VisAttributes* innerXenonAttributes = new G4VisAttributes(G4Colour(0.0, 1.0, 0.0, 0.4)); // Green
+    innerXenonAttributes->SetVisibility(true);
+    innerXenonAttributes->SetLineWidth(2);
+    logicalInnerXenon->SetVisAttributes(innerXenonAttributes);
 
     return physicalWorld;
 }
